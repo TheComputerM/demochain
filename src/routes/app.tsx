cbee7bab44d8d6a4a8ceaad0cedf5b35ac5f1076
TheComputerM <mudit.somani00@gmail.com>
2024-11-17 19:22:20 +0530

    add layout

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -1,5 +1,30 @@
-import type { RouteSectionProps } from "@solidjs/router";
+import { useNavigate, type RouteSectionProps } from "@solidjs/router";
+import { Box, HStack } from "styled-system/jsx";
+import { Button } from "~/components/ui/button";
+import { Tabs } from "~/components/ui/tabs";
 
 export default function AppLayout(props: RouteSectionProps) {
-  return <div>{props.children}</div>;
+	const navigate = useNavigate();
+
+	return (
+		<>
+			<Tabs.Root
+				variant="enclosed"
+				defaultValue={props.location.pathname.split("/").at(-1)}
+				onValueChange={({ value }) => {
+					navigate(`/app/${value}`);
+				}}
+			>
+				<Tabs.List>
+					<Tabs.Trigger value="user">User</Tabs.Trigger>
+					<Tabs.Trigger value="console">Console</Tabs.Trigger>
+					<Tabs.Indicator />
+          <HStack ml="auto">
+            <Button size="sm" variant="subtle">Exit</Button>
+          </HStack>
+				</Tabs.List>
+			</Tabs.Root>
+			{props.children}
+		</>
+	);
 }
