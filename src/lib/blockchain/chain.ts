e0570beeba7c13c0b7c798a90fd8bc3b2e21b34c
TheComputerM <mudit.somani00@gmail.com>
2024-12-22 13:52:42 +0530

    fix: transaction nonce should start with 1

--- src/lib/blockchain/chain.ts
+++ src/lib/blockchain/chain.ts
@@ -46,7 +46,7 @@ export class Blockchain {
 		}
 
 		const transaction = await Transaction.create({
-			nonce: 0,
+			nonce: 1,
 			sender: hexToUint8Array(
 				// pssst, there is a secret message here
 				"6d6f6e65792067726f7773206f6e20746865206d65726b6c652074726565b42a552a010675e0ee6b612e74c73f0af04009ab295772092822b541ac1d34b2a5e0fa",
